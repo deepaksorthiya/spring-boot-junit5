dad7935b65eb53b5ca3ce0509bc7bf357c2f2869
Deepak Kataria <deepak.sorthiya@gmail.com>
2020-01-28 14:25:31 +0530

    CMD update

--- src/main/java/com/example/SpringBootJunit5Application.java
+++ src/main/java/com/example/SpringBootJunit5Application.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJunit5Application {
+public class SpringBootJunit5Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJunit5Application.class, args);
+	}
+	
+	public void run(Strinng ..args){
+		
 	}
 
 }
